98ff8ce2ab64863ee2b568e70ec079c66d7cb1bf
Rohan <rlikhite@gmail.com>
2016-04-05 16:08:14 -0400

    added on/off state icons

--- scripts/background.js
+++ scripts/background.js
@@ -6,7 +6,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     // Send a simple JSON message with true as param
-    chrome.tabs.sendMessage(activeTab.id, {"message": String(_active)});
+    if(_active){
+    	chrome.browserAction.setIcon({path: "../images/enabled.png", tabId:activeTab.id});
+    	chrome.tabs.sendMessage(activeTab.id, {"message": String(_active)});
+    }
+    else{
+    	chrome.browserAction.setIcon({path: "../images/icon.png", tabId:activeTab.id});
+    	chrome.tabs.sendMessage(activeTab.id, {"message": String(_active)});
+    }
+
     _active = !_active;
+    
   });
 });
